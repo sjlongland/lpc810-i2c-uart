9918a24fbeb4e88803b0ca3505cfd619ef367b88
Stuart Longland <me@vk4msl.id.au>
2017-01-01 15:02:37 +1000

    main.c: Add FIFOs, more registers.

--- src/main.c
+++ src/main.c
@@ -29,14 +29,29 @@
 #define DISASSERT_NINTERRUPT \
 	LPC_GPIO_PORT->SET0 = (1 << 1)
 
+/* ----- Bit twiddling helpers ----- */
+
+#define GET_BITS(reg, offset, mask) \
+	((reg >> (offset)) & (mask))
+
+#define SET_BITS(reg, offset, mask, value) \
+	reg = ((reg & ((mask) << (offset))) | ((value) << (offset)))
+
 /* ----- I2C identification data ----- */
 
-/* Registers 0…13 */
-const static char i2c_ident[] = "VK4MSL I2CUART";
+/*! I2C firmware identification string, registers 0…13 */
+const static char reg_ident[] = "VK4MSL I2CUART";
+/*! I2C firmware identification string register start address */
+#define REG_IDENT_ADDR		(0x00)
 
-/* Registers 14 and 15 */
-#define I2C_VERSION_MAJOR	(0)
-#define I2C_VERSION_MINOR	(0)
+/*! Major firmware version number */
+#define REG_VERSION_MAJOR	(0)
+/*! Address of major firmware version number */
+#define REG_VERSION_MAJOR_ADDR	(0x0e)
+/*! Minor firmware version number */
+#define REG_VERSION_MINOR	(0)
+/*! Address of minor firmware version number */
+#define REG_VERSION_MINOR_ADDR	(0x0f)
 
 /* ----- I2C slave state ----- */
 
@@ -68,6 +83,346 @@ uint8_t i2c_reg_addr	= 0;
 
 #define I2C_NEXT 					\
 	LPC_I2C->SLVCTL |= (1 << 0)
+
+/* ----- FIFO definitions ----- */
+
+/*! Size of each FIFO buffer */
+#define FIFO_SZ		(128)
+
+/*!
+ * FIFO data structure
+ */
+struct fifo_t {
+	/*! FIFO data */
+	volatile uint8_t data[FIFO_SZ];
+	/*! Read pointer */
+	volatile uint8_t read_ptr;
+	/*! Write pointer */
+	volatile uint8_t write_ptr;
+};
+
+/*! Transmit FIFO */
+static struct fifo_t tx_fifo;
+
+/*! Receive FIFO */
+static struct fifo_t rx_fifo;
+
+/*!
+ * Reset the FIFO
+ */
+void fifo_reset(struct fifo_t* const fifo) {
+	fifo->read_ptr = 0;
+	fifo->write_ptr = 0;
+}
+
+/*!
+ * Enqueue a byte to the FIFO.
+ * @param	fifo	FIFO to write to
+ * @param	byte	Byte to enqueue
+ * @returns	Number of bytes stored
+ */
+uint8_t fifo_enqueue(struct fifo_t* const fifo, uint8_t byte) {
+	uint8_t next = fifo->write_ptr + 1;
+	if (next == fifo->read_ptr)
+		return 0;	/* FIFO is full */
+	fifo->data[fifo->write_ptr] = byte;
+	fifo->write_ptr = next;
+	return 1;
+}
+
+/*!
+ * Dequeue a byte from the FIFO.
+ * @param	fifo	FIFO to read from
+ * @returns	Byte read
+ * @retval	-1	if empty
+ */
+int16_t fifo_dequeue(struct fifo_t* const fifo) {
+	if (fifo->write_ptr == fifo->read_ptr)
+		return -1;	/* FIFO is empty */
+	return fifo->data[fifo->read_ptr++];
+}
+
+/*!
+ * Return the number of bytes in the FIFO.
+ */
+uint8_t fifo_size(struct fifo_t* const fifo) {
+	return ((fifo->write_ptr - fifo->read_ptr) % FIFO_SZ) + 1;
+}
+
+/* ----- Core software registers ----- */
+
+/*!
+ * Master Status Register.  This indicates the status of the various flags
+ * for the UART peripheral.
+ */
+volatile uint8_t reg_msr = 0;
+
+/*!
+ * Bad configuration supplied.  This bit is set when applying an invalid
+ * configuration, and is cleared by writing a 1 to the corresponding
+ * MST_ACK bit.
+ */
+#define REG_MSR_CFG_ERR		(1 << 7)
+
+/*!
+ * Frame error detected.  A badly formatted frame was encountered (e.g.
+ * parity error, low stop bit, etc.)  Reset by writing this bit to the
+ * MST_ACK register.
+ */
+#define REG_MSR_FRAME_ERR	(1 << 6)
+
+/*!
+ * Break detected.  A "break" signal was detected on the UART.
+ * Reset by writing this bit to the ACK register.
+ */
+#define REG_MSR_BREAK		(1 << 5)
+
+/*!
+ * Transmit interrupt triggered.  See the TX_* registers.
+ */
+#define REG_MSR_TX_INT		(1 << 1)
+
+/*!
+ * Receive interrupt triggered.  See the RX_* registers.
+ */
+#define REG_MSR_RX_INT		(1 << 0)
+
+/*! Master Status Register address */
+#define REG_MSR_ADDR		(0x18)
+
+/*!
+ * Master control register.  This is used to put the UART transceiver
+ * offline, controls the global enabling of interrupts and applies pending
+ * configuration settings.
+ */
+volatile uint8_t reg_mcr = 0;
+/*! Address of major control register */
+#define REG_MCR_ADDR		(0x17)
+
+/*! Apply currently defined settings */
+#define REG_MCR_APPLY		(1 << 7)
+
+/*! Revert to currently active settings */
+#define REG_MCR_REVERT		(1 << 6)
+
+/*!
+ * Enable/Disable interrupts.  When disabled, nINTERRUPT is floating
+ * and may be pulled low by the master before pulsing nRESET to initiate
+ * firmware replacement.
+ */
+#define REG_MCR_IRQ_EN		(1 << 5)
+
+/*!
+ * Enable/Disable transmitter.  When disabled, transmit data is held in
+ * the FIFO until the transmitter is enabled or the FIFO is flushed.  This
+ * can be used to implement handling of hardware or software flow control.
+ */
+#define REG_MCR_TX_EN		(1 << 4)
+
+/*!
+ * Master Status Interrupt Acknowledge register address.
+ * Setting ones here clears corresponding bits in the status register.
+ */
+#define REG_MASTER_ACK_ADDR	(0x15)
+
+/*!
+ * Master Interrupt Enable register
+ */
+volatile uint8_t reg_master_int = 0;
+/*!
+ * Master Interrupt Enable register address.
+ */
+#define REG_MASTER_INT_ADDR	(0x16)
+
+/*!
+ * UART frame configuration register:
+ * - Bits 7…5: Frame size in bits:
+ *   15…9: Reserved
+ *   8: 8-bits
+ *   7: 7-bits
+ *   5…0: Reserved
+ * - Bit 4: Number of stop bits:
+ *   1: 2 bits
+ *   0: 1 bit
+ * - Bit 3…2: Parity mode
+ *   3: Even
+ *   2: Odd
+ *   1: Reserved
+ *   0: None
+ * - Bits 1…0: Reserved
+ */
+uint8_t reg_frame = 0;
+#define REG_FRAME_SZ_OFFSET	(5)		/*!< Frame size bit# */
+#define REG_FRAME_SZ_MASK	(7)		/*!< Frame size mask */
+#define REG_FRAME_SZ_8BIT	(8)		/*!< Frame size = 8 bits */
+#define REG_FRAME_SZ_7BIT	(7)		/*!< Frame size = 7 bits */
+#define REG_FRAME_STOP_OFFSET	(4)		/*!< Stop bit count bit# */
+#define REG_FRAME_STOP_MASK	(1)		/*!< Stop bit count mask */
+#define REG_FRAME_STOP_2BIT	(1)		/*!< 2 stop bits */
+#define REG_FRAME_STOP_1BIT	(1)		/*!< 1 stop bit */
+#define REG_FRAME_PARITY_OFFSET	(2)		/*!< Frame parity bit# */
+#define REG_FRAME_PARITY_MASK	(3)		/*!< Frame parity mask */
+#define REG_FRAME_PARITY_EVEN	(3)		/*!< Even parity */
+#define REG_FRAME_PARITY_ODD	(2)		/*!< Odd parity */
+#define REG_FRAME_PARITY_NONE	(0)		/*!< No parity */
+
+/*! Address of UART frame configuration register */
+#define REG_FRAME_ADDR		(0x14)
+
+/*! UART baud rate register.  Given in big-endian format. */
+uint32_t reg_baud = 0;
+
+/*! Address of baud rate register */
+#define REG_BAUD_ADDR		(0x10)
+
+/*
+ * Receive buffer minimum fill level.  This is the minimum level that will
+ * trigger an interrupt indicating data is waiting.  Triggers the MIN_WAIT
+ * interrupt.
+ */
+uint8_t reg_rx_min = 1;
+/*! Address of Receive buffer minimum fill level register. */
+#define REG_RX_MIN_ADDR		(0x23)
+
+/*
+ * Receive buffer maximum fill level.  This is the maximum level that will
+ * trigger an interrupt indicating the buffer is nearly full.  Triggers the
+ * MAX_WAIT interrupt.
+ */
+uint8_t reg_rx_max = FIFO_SZ;
+/*! Address of Receive buffer minimum fill level register. */
+#define REG_RX_MAX_ADDR		(0x24)
+
+/*! Address of Receive interrupt acknowledge register */
+#define REG_RX_ACK_ADDR		(0x25)
+
+/*! Receive status interrupt enable register */
+uint8_t reg_rx_int = 0;
+/*! Address of Receive interrupt enable register */
+#define REG_RX_INT_ADDR		(0x26)
+
+/*! Address of Receive buffer control register */
+#define REG_RX_CTRL_ADDR	(0x27)
+
+/*! Flush the receive buffer */
+#define REG_RX_CTRL_FLUSH	(1 << 7)
+
+/*! Receive status interrupt enable register */
+uint8_t reg_rx_stat = 0;
+/*! Address of Receive status register */
+#define REG_RX_STAT_ADDR	(0x28)
+
+/*! Address of receive data waiting register */
+#define REG_RX_WAIT_ADDR	(0x29)
+
+/*! Address of receive data buffer free register */
+#define REG_RX_FREE_ADDR	(0x2a)
+
+/*!
+ * Received bytes have been dropped as the FIFO is full.  Clear by
+ * writing this bit to REG_RX_ACK.
+ */
+#define REG_RX_INT_OVER		(1 << 7)
+/*!
+ * The FIFO is now full and further incoming traffic will generate
+ * RX_INT_OVER.  Cleared by:
+ * - writing this bit to RX_ACK
+ * - reading bytes from the FIFO
+ * - emptying the FIFO.
+ */
+#define REG_RX_INT_FULL		(1 << 6)
+/*!
+ * The FIFO has at least RX_MAX bytes stored for reading.  Cleared by:
+ * - writing this bit to RX_ACK
+ * - reading bytes from the FIFO
+ * - emptying the FIFO.
+ */
+#define REG_RX_INT_MAX_WAIT	(1 << 5)
+/*!
+ * The FIFO has at least RX_MIN bytes stored for reading.  Cleared by:
+ * - writing this bit to RX_ACK
+ * - reading bytes from the FIFO
+ * - emptying the FIFO.
+ */
+#define REG_RX_INT_MIN_WAIT	(1 << 4)
+/*!
+ * The FIFO is completely empty.  Cleared by writing this bit to RX_ACK.
+ */
+#define REG_RX_INT_EMPTY	(1 << 3)
+
+/*
+ * Transmit buffer minimum fill level.  This is the minimum level that will
+ * trigger an interrupt indicating data is waiting.  Triggers the MIN_WAIT
+ * interrupt.
+ */
+uint8_t reg_tx_min = 1;
+/*! Address of Transmit buffer minimum fill level register. */
+#define REG_TX_MIN_ADDR		(0x33)
+
+/*
+ * Transmit buffer maximum fill level.  This is the maximum level that will
+ * trigger an interrupt indicating the buffer is nearly full.  Triggers the
+ * MAX_WAIT interrupt.
+ */
+uint8_t reg_tx_max = FIFO_SZ;
+/*! Address of Transmit buffer minimum fill level register. */
+#define REG_TX_MAX_ADDR		(0x34)
+
+/*! Address of Transmit interrupt acknowledge register */
+#define REG_TX_ACK_ADDR		(0x35)
+
+/*! Transmit status interrupt enable register */
+uint8_t reg_tx_int = 0;
+/*! Address of Transmit interrupt enable register */
+#define REG_TX_INT_ADDR		(0x36)
+
+/*! Transmit status interrupt enable register */
+uint8_t reg_tx_ctrl = 0;
+/*! Address of Transmit interrupt enable register */
+#define REG_TX_CTRL_ADDR	(0x37)
+
+/*! Flush the transmit buffer */
+#define REG_TX_CTRL_FLUSH	(1 << 7)
+
+/*! Transmit status interrupt enable register */
+uint8_t reg_tx_stat = 0;
+/*! Address of Transmit status register */
+#define REG_TX_STAT_ADDR	(0x38)
+
+/*! Address of transmit data waiting register */
+#define REG_TX_WAIT_ADDR	(0x39)
+
+/*! Address of transmit data buffer free register */
+#define REG_TX_FREE_ADDR	(0x3a)
+
+/*!
+ * Transmitted bytes have been dropped as the FIFO is full.  Clear by
+ * writing this bit to REG_TX_ACK.
+ */
+#define REG_TX_INT_OVER		(1 << 7)
+/*!
+ * The FIFO is now full and further incoming traffic will generate
+ * TX_INT_OVER.  Cleared by:
+ * - writing this bit to TX_ACK
+ * - emptying the FIFO.
+ */
+#define REG_TX_INT_FULL		(1 << 6)
+/*!
+ * The FIFO has at least TX_MAX bytes stored for writing.  Cleared by:
+ * - writing this bit to TX_ACK
+ * - emptying the FIFO.
+ */
+#define REG_TX_INT_MAX_WAIT	(1 << 5)
+/*!
+ * The FIFO has at least TX_MIN bytes stored for writing.  Cleared by:
+ * - writing this bit to TX_ACK
+ * - emptying the FIFO.
+ */
+#define REG_TX_INT_MIN_WAIT	(1 << 4)
+/*!
+ * The FIFO is completely empty.  Cleared by writing this bit to TX_ACK.
+ */
+#define REG_TX_INT_EMPTY	(1 << 3)
 
 /* ----- Core application ----- */
 
@@ -185,7 +540,7 @@ int main(void) {
 	 * for it to be turned off with a I2C register later.
 	 */
 	DISASSERT_NINTERRUPT;
-	ENABLE_NINTERRUPT(1);
+	ENABLE_NINTERRUPT(0);
 
 	/* ----- I2C setup ----- */
 
@@ -235,17 +590,111 @@ void I2C_IRQHandler(void) {
 		uint8_t byte = 0;
 		i2c_rx_reg_addr = 0;
 		if (!(i2c_reg_addr & 0xf0)) {
+			/* Address 0x00…0x0f */
 			switch(i2c_reg_addr) {
-				case 0x0e:
-					byte = I2C_VERSION_MAJOR;
+				case REG_VERSION_MAJOR_ADDR:
+					byte = REG_VERSION_MAJOR;
 					break;
-				case 0x0f:
-					byte = I2C_VERSION_MINOR;
+				case REG_VERSION_MINOR_ADDR:
+					byte = REG_VERSION_MINOR;
 					break;
 				default:
-					byte = i2c_ident[
+					byte = reg_ident[
 						i2c_reg_addr & 0x0f
 					];
+			}
+		} else if (!(i2c_reg_addr & 0xe0)) {
+			/* Address 0x10…0x1f */
+			switch(i2c_reg_addr) {
+				case REG_MCR_ADDR:
+					byte = reg_mcr;
+					break;
+				case REG_MSR_ADDR:
+					byte = reg_msr;
+					break;
+				case REG_MASTER_ACK_ADDR:
+					byte = reg_msr;
+					break;
+				case REG_MASTER_INT_ADDR:
+					byte = reg_master_int;
+					break;
+				case REG_FRAME_ADDR:
+					byte = reg_frame;
+					break;
+				case REG_BAUD_ADDR:
+					byte = GET_BITS(reg_baud,
+							24, 0xff);
+					break;
+				case REG_BAUD_ADDR+1:
+					byte = GET_BITS(reg_baud,
+							16, 0xff);
+					break;
+				case REG_BAUD_ADDR+2:
+					byte = GET_BITS(reg_baud,
+							8, 0xff);
+					break;
+				case REG_BAUD_ADDR+3:
+					byte = GET_BITS(reg_baud,
+							0, 0xff);
+					break;
+				default:
+					byte = 0xff;
+			}
+		} else if ((i2c_reg_addr & 0xf0) == 0x20) {
+			/* Address 0x20…0x2f */
+			switch(i2c_reg_addr) {
+				case REG_RX_MIN_ADDR:
+					byte = reg_rx_min;
+					break;
+				case REG_RX_MAX_ADDR:
+					byte = reg_rx_max;
+					break;
+				case REG_RX_ACK_ADDR:
+					byte = reg_rx_stat;
+					break;
+				case REG_RX_STAT_ADDR:
+					byte = reg_rx_stat;
+					break;
+				case REG_RX_INT_ADDR:
+					byte = reg_rx_int;
+					break;
+				case REG_RX_WAIT_ADDR:
+					byte = fifo_size(&rx_fifo);
+					break;
+				case REG_RX_FREE_ADDR:
+					byte = FIFO_SZ
+						- fifo_size(&rx_fifo);
+					break;
+				default:
+					byte = 0xff;
+			}
+		} else if ((i2c_reg_addr & 0xf0) == 0x30) {
+			/* Address 0x30…0x3f */
+			switch(i2c_reg_addr) {
+				case REG_TX_MIN_ADDR:
+					byte = reg_tx_min;
+					break;
+				case REG_TX_MAX_ADDR:
+					byte = reg_tx_max;
+					break;
+				case REG_TX_ACK_ADDR:
+					byte = reg_tx_stat;
+					break;
+				case REG_TX_STAT_ADDR:
+					byte = reg_tx_stat;
+					break;
+				case REG_TX_INT_ADDR:
+					byte = reg_tx_int;
+					break;
+				case REG_TX_WAIT_ADDR:
+					byte = fifo_size(&tx_fifo);
+					break;
+				case REG_TX_FREE_ADDR:
+					byte = FIFO_SZ
+						- fifo_size(&tx_fifo);
+					break;
+				default:
+					byte = 0xff;
 			}
 		} else if (!(i2c_reg_addr & 0x80)) {
 			/* TODO */
@@ -266,9 +715,98 @@ void I2C_IRQHandler(void) {
 		} else {
 			/* Write the incoming byte to the given address */
 			if (!(i2c_reg_addr & 0xf0)) {
-				/* Read only addresses */
+				/* Address 0x00…0x0f: Read only */
 				I2C_NAK;
 				goto done;
+			} else if (!(i2c_reg_addr & 0xe0)) {
+				/* Address 0x10…0x1f */
+				switch(i2c_reg_addr) {
+					case REG_MCR_ADDR:
+						reg_mcr =
+							LPC_I2C->SLVDAT;
+						break;
+					case REG_MASTER_ACK_ADDR:
+						reg_msr &=
+							~LPC_I2C->SLVDAT;
+						break;
+					case REG_MASTER_INT_ADDR:
+						reg_master_int =
+							LPC_I2C->SLVDAT;
+						break;
+					case REG_FRAME_ADDR:
+						reg_frame =
+							LPC_I2C->SLVDAT;
+						break;
+					case REG_BAUD_ADDR:
+						SET_BITS(reg_baud,
+							24, 0xff,
+							LPC_I2C->SLVDAT);
+						break;
+					case REG_BAUD_ADDR+1:
+						SET_BITS(reg_baud,
+							16, 0xff,
+							LPC_I2C->SLVDAT);
+						break;
+					case REG_BAUD_ADDR+2:
+						SET_BITS(reg_baud,
+							8, 0xff,
+							LPC_I2C->SLVDAT);
+						break;
+					case REG_BAUD_ADDR+3:
+						SET_BITS(reg_baud,
+							0, 0xff,
+							LPC_I2C->SLVDAT);
+						break;
+					default:
+						I2C_NAK;
+						goto done;
+				}
+			} else if ((i2c_reg_addr & 0xf0) == 0x20) {
+				/* Address 0x20…0x2f */
+				switch(i2c_reg_addr) {
+					case REG_RX_MIN_ADDR:
+						reg_rx_min =
+							LPC_I2C->SLVDAT;
+						break;
+					case REG_RX_MAX_ADDR:
+						reg_rx_max =
+							LPC_I2C->SLVDAT;
+						break;
+					case REG_RX_ACK_ADDR:
+						reg_rx_stat &=
+							~LPC_I2C->SLVDAT;
+						break;
+					case REG_RX_INT_ADDR:
+						reg_rx_int =
+							LPC_I2C->SLVDAT;
+						break;
+					default:
+						I2C_NAK;
+						goto done;
+				}
+			} else if ((i2c_reg_addr & 0xf0) == 0x30) {
+				/* Address 0x30…0x3f */
+				switch(i2c_reg_addr) {
+					case REG_TX_MIN_ADDR:
+						reg_tx_min =
+							LPC_I2C->SLVDAT;
+						break;
+					case REG_TX_MAX_ADDR:
+						reg_tx_max =
+							LPC_I2C->SLVDAT;
+						break;
+					case REG_TX_ACK_ADDR:
+						reg_tx_stat &=
+							~LPC_I2C->SLVDAT;
+						break;
+					case REG_TX_INT_ADDR:
+						reg_tx_int;
+							LPC_I2C->SLVDAT;
+						break;
+					default:
+						I2C_NAK;
+						goto done;
+				}
 			} else if (!(i2c_reg_addr & 0x80)) {
 				/* TODO */
 				I2C_NAK;
